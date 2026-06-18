1f69eebef9b3cb0c5f25921c24dcc0a961f6002a
MuhammadMaruf11 <muhammad11maruf@gmail.com>
2026-06-18 08:41:47 +0600

    chore: initial project setup (backend + frontend)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-red-500">Hi, welcome to Sneaker Drop!</h1>
+      <p className="text-xl text-gray-700">This is a React app.</p>
+    </>
+  );
+}
+
+export default App;
